e02caa43660002f343355fe2c66adb0a1465aa16
v.peskarski <v.peskarskij@gmail.com>
2022-12-04 13:36:02 +0100

    feat: resolve wt and cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+import { getDirname } from '../utils/index.js';
+
+const __dirname = getDirname(import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+   const child = fork(`${__dirname}/files/script.js`, args, { stdio: 'pipe' });
+
+   process.stdin.pipe(child.stdin);
+   child.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2']);
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,17 +1,26 @@
-import fs from 'fs/promises';
-import { existsSync } from 'fs';
+import fs, { access, constants } from 'fs/promises';
 import { getDirname } from '../utils/index.js';
 
 const __dirname = getDirname(import.meta.url);
 
 const rename = async () => {
     const directory = `${__dirname}/files`;
+    
 
-    if (existsSync(`${directory}/properFilename.md`)) {
-        throw new Error('FS operation failed');
+    const isFileExisting = async (path) => {
+        try {
+            await fs.access(path, constants.F_OK);
+            return true;
+        } catch {
+            return false;
+        }
     }
 
     try {
+        if (await isFileExisting(`${directory}/properFilename.md`)) {
+            throw new Error('FS operation failed');
+        }
+
         await fs.rename(`${directory}/wrongFilename.txt`, `${directory}/properFilename.md`);
     } catch (err) {
         if (err.code === 'ENOENT') {
